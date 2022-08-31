923f4b786c15109b426004bdc57217028ba25c22
Mary Rossi <maryrossi106@gmail.com>
2022-08-31 00:12:39 -0500

    drinks index

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const drinks = require("./models/drinks.js")
 const app = express();
 const port = 3000;
 
+//setting view engine to ejs
+// app.set("view engine", "ejs");
+
 app.get("/", (req, res)=>{
     res.send("Welcome to the Gitpub App!")
 });
 
 app.get("/drinks/", (req, res)=>{
-    res.send(drinks)
+    res.render("drinks_index.ejs", {
+        allDrinks: drinks
+    })
 });
 
-// app.get("/drinks/:indexOfDrinksArray", (req, res)=>{
-//     res.render("")
-// })
+app.get("/drinks/:indexOfDrinksArray", (req, res)=>{
+    res.render("drinks_show.ejs", {
+        drink: drinks[req.indexOfDrinksArray]
+    })
+})
 
 app.listen(port, ()=>{
     console.log(`listening on port`, port)
