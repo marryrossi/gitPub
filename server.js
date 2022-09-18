f78096df6106318e8c20026a15cdaec482fc0463
Mary Rossi <maryrossi106@gmail.com>
2022-09-18 15:28:26 -0500

    added & fixed drinks menu

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
+// Dependencies
 const express = require("express");
-const drinks = require("./models/drinks.js")
-
 const app = express();
+const drinks = require("./models/drinks.js");
 const port = 3000;
 
+//Routes
+// Greetings
 app.get("/", (req, res)=>{
-    res.send("Welcome to the Gitpub App!")
+    res.send("Welcome to the gitPub App!")
 });
 
-app.get("/drinks/", (req, res)=>{
+// Drink names
+app.get("/drinks", (req, res)=>{
     res.render("drinks_index.ejs", {
-        allDrinks: drinks
-    })
+        allDrinks: drinks,
+    });
 });
 
-app.get("/drinks/:indexOfDrinksArray", (req, res)=>{
+// Drink ID
+app.get("/drinks/:id", (req, res)=>{
     res.render("drinks_show.ejs", {
-        drink: drinks[req.params.indexOfDrinksArray]
-    })
-})
+        drinkInfo: drinks[req.params.id]
+    });
+});
 
+// Listening
 app.listen(port, ()=>{
     console.log(`listening on port`, port)
 });
-
